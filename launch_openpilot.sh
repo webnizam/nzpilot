5ee771b9e0aa721a4d02dc6f18e0b96c070db112
Nizamuddin Mohamed <webnizam@gmail.com>
2022-06-10 18:11:19 +0400

    added liveparam auto deletion

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# I find the car drive the best after a fresh LiveParameters deletion, from straights to curves
+while true; do rm -rf /data/params/d/LiveParameters; sleep 30; done &
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
